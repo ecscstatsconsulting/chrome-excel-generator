be24359c0747c5ea82c213ab7dbe01f1416bd108
ECSC, ltd <33598754+ecscstatsconsulting@users.noreply.github.com>
2018-02-05 20:47:26 -0500

    Added comments

--- scripts/content.js
+++ scripts/content.js
@@ -29,59 +29,65 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 Author Paul Warren */
 
 /** 
- *Inform the background page that 
-*/ this tab should have a page-action
+ * Inform the background page that 
+ * this tab should have a page-action
+ */
 chrome.runtime.sendMessage({
     from:    'content',
     subject: 'showPageAction'
-  });
-  
-  // Listen for messages from the popup
-  chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-    // First, validate the message's structure
-    if (msg.from === 'popup') {
-      switch(msg.subject) {
-        case "loadTables":
-          var tables = [];
-          var counter = 1;
-          $("table:visible").each(function() {
-              var name = "Table " + counter;
-              $(this).attr("__EXCEL_TABLE_NAME__", name);
-              var id = counter++;
-              var html = this.outerHTML;
-              var table = {
-                "id": id,
-                "name": name,
-                "html": html
-              };
-              tables.push(table);
-          });
-          var tableInfo = {
-            "tables": tables
-          };
-      
-          response(tableInfo);
-          break;
-        case "highlight":
-          var tbl = $("[__EXCEL_TABLE_NAME__='" + msg.tableName + "']");
-          switch (msg.onoff)
-          {
-            case "on":
-              tbl.attr("__EXCEL_TABLE_ORIG_BORDER__", (tbl.css("border")) ? tbl.css("border") : "0px solid black");
-              tbl.css("border","5px solid green");
-              $('html, body').stop().animate({
-                scrollTop: (tbl.offset().top - 50)
-              },500);
-              break;
-            case "off":
-              tbl.css("border", tbl.attr("__EXCEL_TABLE_ORIG_BORDER__"));
-              break;
-          }
-          
-          break;
-      }
- 
+});
 
+/**
+ * Listen for messages from the popup
+ */
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+  if (msg.from === 'popup') {
+    switch(msg.subject) {
+      case "loadTables":
+        //Find valid tables and pass them back to the popup
+        var tables = [];
+        var counter = 1;
+        $("table:visible").each(function() {
+            var name = "Table " + counter;
+            //Adding attribute to the table for identification later.
+            $(this).attr("__EXCEL_TABLE_NAME__", name);
+            var id = counter++;
+            var html = this.outerHTML;
+            //create return object with all the info needed by the popup
+            var table = {
+              "id": id,
+              "name": name,
+              "html": html
+            };
+            tables.push(table);
+        });
+        var tableInfo = {
+          "tables": tables
+        };
+        //return all of the table information from the active tab
+        response(tableInfo);
+        break;
+      case "highlight":
+        //Scroll to and highlight selected table
+        var tbl = $("[__EXCEL_TABLE_NAME__='" + msg.tableName + "']");
+        switch (msg.onoff)
+        {
+          case "on":
+            //highlight table and scroll to it
+            tbl.attr("__EXCEL_TABLE_ORIG_BORDER__", (tbl.css("border")) ? tbl.css("border") : "0px solid black");
+            tbl.css("border","5px solid green");
+            $('html, body').stop().animate({
+              scrollTop: (tbl.offset().top - 50)
+            },500);
+            break;
+          case "off":
+            //remove highlighting from the table.
+            tbl.css("border", tbl.attr("__EXCEL_TABLE_ORIG_BORDER__"));
+            break;
+        }
+          
+    break;
     }
-  });
+  }
+});
 
