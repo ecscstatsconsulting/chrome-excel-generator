7b04df424991b11f33948b9adc7976d93832befb
ECSC, ltd <33598754+ecscstatsconsulting@users.noreply.github.com>
2018-02-05 20:50:52 -0500

    Added Comments

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,31 @@
+/* background.js
+
+Background script which creates the popu and connects it to the active tab.
+
+---------------
+- MIT License -
+---------------
+Copyright 2018 ECSC, ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author Paul Warren */
+
 /**
  * Listens for the app launching, then creates the window.
  *
@@ -14,6 +42,9 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   );
 });
 
+/**
+ * Create the connection between the active tab and the popup
+ */
 chrome.runtime.onMessage.addListener(function (msg, sender) {
   // First, validate the message's structure
   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
