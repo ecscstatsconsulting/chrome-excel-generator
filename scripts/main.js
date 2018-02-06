067f0d3ef779ed347852edfa532cfa2757719fa0
paul0warren <paul0warren3@gmail.com>
2018-02-06 09:23:28 -0500

    Added Google Analytics

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,19 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 Author Paul Warren */
 
 /**
+ * Standard Google Analytics
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-113470385-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+/**
  * Takes table information gathered from the Active Tab and populates the popup window.
  */
 function setTableInfo(info) {
