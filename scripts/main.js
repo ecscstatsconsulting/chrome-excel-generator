4dd8f6ac159b1fb369ab2e61ac140348052a4ce6
ECSC, ltd <33598754+ecscstatsconsulting@users.noreply.github.com>
2018-02-05 20:37:36 -0500

    Added Comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,39 @@
+/*require excel-gen.js jszip.js FileSaver.js jquery*/
 
-document.addEventListener('DOMContentLoaded', function () {
+/* main.js
 
-});
+Script for the functionality of the Chrome Extension popup window.
 
-// Update the relevant fields with the new data
-function setDOMInfo(info) {
+---------------
+- MIT License -
+---------------
+Copyright 2018 ECSC, ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author Paul Warren */
+
+/**
+ * Takes table information gathered from the Active Tab and populates the popup window.
+ */
+function setTableInfo(info) {
   if (info && info.tables && info.tables.length) {
+    //create list of tables
     var arr = [];
     arr.push("<ul>");
     info.tables.forEach(v => {
@@ -15,6 +43,8 @@ function setDOMInfo(info) {
     });
     arr.push("</ul>");
     $("#content").html(arr.join(""));
+    
+    //Add hover effect
     $("#content ul li").hover(
       function(){
         highlightTable($(this).text(), "on");
@@ -23,6 +53,8 @@ function setDOMInfo(info) {
         highlightTable($(this).text(), "off");
       }
     );
+    
+    //Create Excel spreadsheet on clicking
     $("#content ul li").click(function () {
       var tbl_name = $(this).text();
       var tbl = info.tables.find((itm) => itm.name == tbl_name);
@@ -40,8 +72,11 @@ function setDOMInfo(info) {
   
 }
 
+/**
+ * Highlight the Table on the popup and make a call to scroll to the table
+ * on the active tab and draw a border around it
+ */
 function highlightTable(tableName, onoff) {
-  console.log("highlight " + tableName + " " + onoff);
   chrome.tabs.query({
     active: true,
     currentWindow: true
@@ -54,14 +89,19 @@ function highlightTable(tableName, onoff) {
   });
 };
 
+/**
+ * Call to the active tab to get a list of all HTML tables
+ */
 function loadTables(tab) {
   chrome.tabs.sendMessage(
     tab.id,
     {from: 'popup', subject: 'loadTables'},
-    setDOMInfo);
+    setTableInfo);
 };
 
-// Once the DOM is ready...
+/**
+ * Popup loaed event, make call to list tables on the active tab
+ */
 window.addEventListener('DOMContentLoaded', function () {
   // ...query for the active tab...
   chrome.tabs.query({
